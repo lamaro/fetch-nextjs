2f04b5404367bd7f31f096471dc4a2fca146fb95
Leandro Amaro Garcia <>
2019-12-04 18:35:03 -0300

    Initial

---
+++ .next/static/webpack/static/development/pages/index.js.50959637a39dd6720127.hot-update.js
@@ -0,0 +1,65 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Layout/index.js":
+/*!************************************!*\
+  !*** ./components/Layout/index.js ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../Header */ "./components/Header/index.js");
+var _jsxFileName = "/Users/lean/Documents/APPS/NEXTJS/hola-next/components/Layout/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+};
+
+var Layout = function Layout(props) {
+  return __jsx("div", {
+    style: layoutStyle,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, __jsx(_Header__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }), props.children);
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Layout);
+
+/***/ }),
+
+/***/ "./node_modules/process/browser.js":
+false,
+
+/***/ "./node_modules/string-hash/index.js":
+false,
+
+/***/ "./node_modules/styled-jsx/dist/lib/stylesheet.js":
+false,
+
+/***/ "./node_modules/styled-jsx/dist/style.js":
+false,
+
+/***/ "./node_modules/styled-jsx/dist/stylesheet-registry.js":
+false,
+
+/***/ "./node_modules/styled-jsx/style.js":
+false
+
+})
+//# sourceMappingURL=index.js.50959637a39dd6720127.hot-update.js.map
